446a6fa9b59954a720c59a7a6f9dcdc442a9fc3e
Josh <you@example.coexit>
2016-03-15 13:05:33 -0500

    stage 3

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,21 @@
 var counter = 0;
 
-var likes = document.querySelector('button');
+var likes = document.querySelector('.likeButton');
+var doReset = document.querySelector('.resetButton');
 
-likes.addEventListener('click',addLikes);
-
-// button.addEventListener('click', addLikes);
+likes.addEventListener('click', addLikes);
+doReset.addEventListener('click', resetNow);
 
 function addLikes(){
-	counter++
-	likes.innerHTML = counter + ' Likes';
+    counter++;
+    if (counter === 1) {
+        likes.innerHTML = counter + ' Like';
+    } else {
+        likes.innerHTML = counter + ' Likes';
+    }
 }
 
-
-
- 
-
+function resetNow() {
+    counter = 0;
+    likes.innerHTML = '0 Likes';
+}
